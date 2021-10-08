aed4389d0af9c0466ca118ddbba7f01f5fddef9b
yzhilong <yzlong99@gmail.com>
2021-10-08 18:55:16 +0800

    Fix bug with DateFormat

--- src/main/java/seedu/address/model/gameentry/DatePlayed.java
+++ src/main/java/seedu/address/model/gameentry/DatePlayed.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 public class DatePlayed {
 
-    private static DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-mm-dd hh:mm");
+    private static DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd hh:mm");
     private Date datePlayed;
 
     public DatePlayed() {
@@ -18,8 +18,7 @@ public class DatePlayed {
     }
 
     public DatePlayed(Date date) {
-        long millisecondsSinceEpochStart = date.getTime();
-        this.datePlayed = new Date(millisecondsSinceEpochStart);
+        this.datePlayed = date;
     }
 
     /**
