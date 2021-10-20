4fd9944a2fbffec7aaf5c3872df4443a711f4d00
shaliniseshadri <77195499+shaliniseshadri@users.noreply.github.com>
2021-10-21 01:41:15 +0800

    Change the DS used to process the data for Average Profit

--- src/main/java/seedu/address/model/stats/StatsByDate.java
+++ src/main/java/seedu/address/model/stats/StatsByDate.java
@@ -8,34 +8,34 @@ import seedu.address.model.gameentry.GameEntry;
 import java.util.*;
 
 public class StatsByDate {
-	private static List<Tuple> averageSeries = new ArrayList<>();
+	private static HashMap<DatePlayed, List<Double>> preprocessedDates = new HashMap<>();
+	private static HashMap<String, Double> processedDates = new HashMap<>();
+	private static List<GameEntry> gameEntryList = new ArrayList<>();
 
 	private StatsByDate() {
 	}
 
-	public static List<Tuple> getStats(ObservableList<GameEntry> gameEntries) {
-		gameEntries.forEach(gameEntry -> averageSeries.add( new Tuple(gameEntry.getDate(), gameEntry.getProfit())));
-		return averageSeries;
+	public static HashMap<String, Double> getStats() {
+		// BUG: Need to fix the duplicate handling here
+			System.out.println(gameEntryList);
+			gameEntryList.forEach(gameEntry -> {
+						if (!preprocessedDates.containsKey(gameEntry.getDate())) {
+							preprocessedDates.put(gameEntry.getDate(), new ArrayList<Double>());
+						}
+						preprocessedDates.get(gameEntry.getDate()).add(gameEntry.getProfit());
+
+					}
+			);
+			System.out.println(preprocessedDates);
+			preprocessedDates.forEach((date, listOfProfits) -> {
+				Double averageProfit = listOfProfits.stream().mapToDouble(Double::doubleValue).average().orElse(0.00);
+				processedDates.put(date.toString().strip().split("\\s", 2)[0], averageProfit);
+			});
+
+		return processedDates;
 	}
 
-	public static class Tuple
-	{
-		DatePlayed datePlayed;
-		Double profit;
-		public Tuple(DatePlayed datePlayed, Double profit) {
-			this.datePlayed = datePlayed;
-			this.profit = profit;
-		}
-
-		public String getDate() {
-			return this.datePlayed.toString().strip().split("\\s", 2)[0];
-		}
-
-		public Double getProfit() {
-			return this.profit;
-		}
-
+	public static void initList(ObservableList<GameEntry> list) {
+		gameEntryList = list;
 	}
-
-
 }
--- src/main/java/seedu/address/ui/GraphPanel.java
+++ src/main/java/seedu/address/ui/GraphPanel.java
@@ -1,13 +1,13 @@
 package seedu.address.ui;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.Region;
+import seedu.address.model.gameentry.DatePlayed;
 import seedu.address.model.gameentry.GameEntry;
 import seedu.address.model.stats.StatsByDate;
 
@@ -19,40 +19,39 @@ public class GraphPanel extends UiPart<Region> {
 
 	private ObservableList<GameEntry> gameEntryList;
 	private XYChart.Series series;
-	private List<StatsByDate.Tuple> averageSeries;
+	private HashMap<String, Double> averageProfits;
 
 	// todo:
 	// Bugs:
 	// Refreshing axes while updating the list - currently only the series is refreshed and not the axes
+	// Duplicate handling in StatsByDate
 
 	public GraphPanel(ObservableList<GameEntry> gameEntryList) {
 		super(FXML);
 		this.gameEntryList = gameEntryList;
+		StatsByDate.initList(gameEntryList);
 		series = new XYChart.Series();
 		series.setName("Average");
 	}
 
 	public void drawGraph() {
-		averageSeries = StatsByDate.getStats(gameEntryList);
-		Comparator<StatsByDate.Tuple> c = Comparator.comparing(StatsByDate.Tuple::getDate);
-		averageSeries.sort(c);
-		for (StatsByDate.Tuple entry : averageSeries) {
-			System.out.println(entry.getDate());
-			System.out.println(entry.getProfit());
-			series.getData().add(new XYChart.Data(entry.getDate(), entry.getProfit()));
+		averageProfits = StatsByDate.getStats();
+		Map<String, Double> sortedAverageProfits = new TreeMap<>(averageProfits); //sorts the hashmap
+		for (Map.Entry<String, Double> entry : sortedAverageProfits.entrySet()) {
+				series.getData().add(new XYChart.Data(entry.getKey().toString(), entry.getValue()));
 		}
 		lineChart.getData().add(series);
 	}
 
 	public void updateList(ObservableList<GameEntry> list) {
 		this.gameEntryList = list;
+		StatsByDate.initList(list);
 		this.drawGraph();
 	}
 
 	public void clear() {
 		series.getData().clear();
-		averageSeries.clear();
-		lineChart.getXAxis().autosize();
+		averageProfits.clear();
 	}
 }
 
