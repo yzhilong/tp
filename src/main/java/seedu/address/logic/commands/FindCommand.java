e0d6faa0bc2922154b55d172e8518bde36fdc534
dannylty <dannylty98@gmail.com>
2021-10-22 01:42:10 +0800

    Add FindCommand

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -7,30 +7,31 @@ import seedu.address.model.Model;
 import seedu.address.model.gameentry.GameEntryContainsKeywordPredicate;
 
 /**
- * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Finds and lists all game entries in game book whose contents matches
+ * any given argument keyword. Matches game name, location, or tags.
  * Keyword matching is case insensitive.
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all game entries whose contents contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "Example: " + COMMAND_WORD + " friends mbs";
 
-    private final NameContainsKeywordsPredicate predicate;
+    private final GameEntryContainsKeywordPredicate predicate;
 
-    public FindCommand(NameContainsKeywordsPredicate predicate) {
+    public FindCommand(GameEntryContainsKeywordPredicate predicate) {
         this.predicate = predicate;
     }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredPersonList(predicate);
+        model.updateFilteredGameEntryList(predicate);
         return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+                String.format(Messages.MESSAGE_GAMEENTRIES_LISTED_OVERVIEW, model.getFilteredGameEntryList().size()));
     }
 
     @Override
