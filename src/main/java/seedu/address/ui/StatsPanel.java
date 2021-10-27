f4532d446ac6e2d02866c80e7a4ad810921f89a5
shaliniseshadri <77195499+shaliniseshadri@users.noreply.github.com>
2021-10-28 00:36:38 +0800

    Represent Table as a Text card

--- src/main/java/seedu/address/ui/StatsPanel.java
+++ src/main/java/seedu/address/ui/StatsPanel.java
@@ -1,34 +1,21 @@
 package seedu.address.ui;
 
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
-import javafx.util.Pair;
 import seedu.address.model.gameentry.GameEntry;
 import seedu.address.model.stats.Average;
 import seedu.address.model.stats.Median;
 
 public class StatsPanel extends UiPart<Region> {
     private static final String FXML = "StatsPanel.fxml";
-    private static final String MEAN = "Mean";
-    private static final String MEDIAN = "Median";
-    private ObservableList<GameEntry> gameEntries;
-
+    private ObservableList<GameEntry> gameEntryList;
+    @FXML
+    private Label mean;
 
     @FXML
-    private TableView<Pair<String, String>> table;
-
-    @FXML
-    private TableColumn<Pair<String, String>, String> statName;
-
-    @FXML
-    private TableColumn<Pair<String, String>, String> value;
-
-    private ObservableList<Pair<String, String>> gameEntryList = FXCollections.observableArrayList();
+    private Label median;
 
     /**
      * Constructor used to initialise a StatsPanel using a given GameEntry list
@@ -36,20 +23,12 @@ public class StatsPanel extends UiPart<Region> {
      */
     public StatsPanel(ObservableList<GameEntry> gameEntries) {
         super(FXML);
-        this.gameEntries = gameEntries;
+        this.gameEntryList = gameEntries;
     }
 
     public void getStats() {
-        String overallAverage = String.format("%.2f", Average.getOverallAverage(gameEntries));
-        String overallMedian = String.format("%.2f", Median.getOverallMedian(gameEntries));
-
-        gameEntryList.add(new Pair<>(MEAN, overallAverage));
-        gameEntryList.add(new Pair<>(MEDIAN, overallMedian));
-
-        statName.setCellValueFactory(new PropertyValueFactory<>("key"));
-        value.setCellValueFactory(new PropertyValueFactory<>("value"));
-
-        table.setItems(gameEntryList);
+        this.mean.setText(String.format("%.2f", Average.getOverallAverage(gameEntryList)));
+        this.median.setText(String.format("%.2f", Median.getOverallMedian(gameEntryList)));
     }
 
     /**
@@ -57,8 +36,7 @@ public class StatsPanel extends UiPart<Region> {
      * @param gameEntries
      */
     public void updateStats(ObservableList<GameEntry> gameEntries) {
-        gameEntryList.clear();
-        this.gameEntries = gameEntries;
+        this.gameEntryList = gameEntries;
         this.getStats();
     }
 
