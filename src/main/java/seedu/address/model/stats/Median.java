07e374cf5293d21a28f1eacab4ca9e21252448f8
shaliniseshadri <77195499+shaliniseshadri@users.noreply.github.com>
2021-10-27 23:04:36 +0800

    Fix illegal argument value bug

--- src/main/java/seedu/address/model/stats/Median.java
+++ src/main/java/seedu/address/model/stats/Median.java
@@ -7,17 +7,21 @@ import seedu.address.model.gameentry.GameEntry;
 
 public class Median {
 
-    private Median () {
+    private Median() {
     }
 
     public static Double getOverallMedian(List<GameEntry> gameEntryList) {
-        double overallMedian;
-        DoubleStream sortedProfitList = gameEntryList.stream().mapToDouble(GameEntry::getDifference).sorted();
-        if (gameEntryList.size() % 2 == 0) {
-            overallMedian = sortedProfitList.skip((gameEntryList.size() / 2) - 1).limit(2).average().orElse(0.00);
-        } else {
-            overallMedian = sortedProfitList.skip(gameEntryList.size() / 2).findFirst().orElse(0.00);
+        try {
+            double overallMedian;
+            DoubleStream sortedProfitList = gameEntryList.stream().mapToDouble(GameEntry::getDifference).sorted();
+            if (gameEntryList.size() % 2 == 0) {
+                overallMedian = sortedProfitList.skip((gameEntryList.size() / 2) - 1).limit(2).average().orElse(0.00);
+            } else {
+                overallMedian = sortedProfitList.skip(gameEntryList.size() / 2).findFirst().orElse(0.00);
+            }
+            return overallMedian;
+        } catch (IllegalArgumentException e) {
+            return 0.00;
         }
-        return overallMedian;
     }
 }
