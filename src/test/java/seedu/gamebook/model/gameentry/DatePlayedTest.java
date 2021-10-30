27aebd32bb9ca49f84b345e55188cbfaee4d536e
yzhilong <yzlong99@gmail.com>
2021-10-31 00:19:44 +0800

    Update unit tests

--- src/test/java/seedu/gamebook/model/gameentry/DatePlayedTest.java
+++ src/test/java/seedu/gamebook/model/gameentry/DatePlayedTest.java
@@ -1,5 +1,6 @@
 package seedu.gamebook.model.gameentry;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.gamebook.testutil.Assert.assertThrows;
@@ -10,97 +11,91 @@ public class DatePlayedTest {
 
     @Test
     public void constructor_invalidDatetime_throwsIllegalArgumentException() {
-        // Invalid date
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("123/12/12 12:12"));
+        // Valid datetime
+        assertDoesNotThrow(() -> new DatePlayed("2012-12-12 12:12"));
 
         // Invalid date
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("32/12/12 12:12"));
+        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("2012-12-123 12:12"));
+
+        // Invalid date
+        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("2012-12-32 12:12"));
 
         // Invalid month
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("12/123/12 12:12"));
+        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("2012-32-12 12:12"));
 
         // Invalid month
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("12/0/12 12:12"));
+        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("2012-0-12 12:12"));
 
         // Invalid month
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("12/13/12 12:12"));
-
-        // Invalid year
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("12/12/123 12:12"));
-
-        // Invalid year
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("12/12/0 12:12"));
+        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("2012-13-12 12:12"));
 
         // Invalid hour
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("12/12/12 123:12"));
+        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("2012-12-12 123:12"));
 
         // Invalid hour
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("12/12/12 25:12"));
+        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("2012-12-12 25:12"));
 
         // Invalid minute
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("12/12/12 12:123"));
+        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("2012-12-12 12:123"));
 
         // Invalid minute
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("12/12/12 25:60"));
+        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("2012-12-12 25:60"));
     }
 
     @Test
     public void constructor_invalidDate_throwsIllegalArgumentException() {
-        // Invalid date
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("123/12/12"));
+        // Valid date
+        assertDoesNotThrow(() -> new DatePlayed("2012-12-12"));
 
         // Invalid date
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("32/12/12"));
+        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("2012-12-123"));
+
+        // Invalid date
+        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("2012-12-32"));
 
         // Invalid month
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("12/123/12"));
+        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("2012-123-12"));
 
         // Invalid month
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("12/0/12"));
+        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("2012-0-12"));
 
         // Invalid month
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("12/13/12"));
-
-        // Invalid year
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("12/12/123"));
-
-        // Invalid year
-        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("12/12/0"));
+        assertThrows(IllegalArgumentException.class, () -> new DatePlayed("2012-13-12"));
     }
 
     @Test
     public void equals_condition_equalIfSameMinute() {
         // Exact same time
-        DatePlayed date0 = new DatePlayed("05/10/21 11:09");
-        DatePlayed date1 = new DatePlayed("05/10/21 11:09");
+        DatePlayed date0 = new DatePlayed("2021-05-10 11:09");
+        DatePlayed date1 = new DatePlayed("2021-05-10 11:09");
         assertEquals(date0, date1);
 
         // Different minute
-        date0 = new DatePlayed("05/10/21 11:09");
-        date1 = new DatePlayed("05/10/21 11:06");
+        date0 = new DatePlayed("2021-05-10 11:09");
+        date1 = new DatePlayed("2021-05-10 11:06");
         assertNotEquals(date0, date1);
     }
 
     @Test
     public void equals_condition_equalIfSameDate() {
         // Same date
-        DatePlayed date0 = new DatePlayed("05/10/21");
-        DatePlayed date1 = new DatePlayed("05/10/21");
+        DatePlayed date0 = new DatePlayed("2021-05-10");
+        DatePlayed date1 = new DatePlayed("2021-05-10");
         assertEquals(date0, date1);
 
         // Different date
-        date0 = new DatePlayed("02/10/21");
-        date1 = new DatePlayed("05/10/21");
+        date0 = new DatePlayed("2021-05-02");
+        date1 = new DatePlayed("2021-05-10");
         assertNotEquals(date0, date1);
 
         // Different month
-        date0 = new DatePlayed("05/11/21");
-        date1 = new DatePlayed("05/10/21");
+        date0 = new DatePlayed("2021-05-10");
+        date1 = new DatePlayed("2021-06-10");
         assertNotEquals(date0, date1);
 
         // Different year
-        date0 = new DatePlayed("05/10/20");
-        date1 = new DatePlayed("05/10/21");
+        date0 = new DatePlayed("2020-05-10");
+        date1 = new DatePlayed("2021-05-10");
         assertNotEquals(date0, date1);
     }
 
@@ -110,8 +105,8 @@ public class DatePlayedTest {
         DatePlayed date1;
 
         // Same date, but one of them has time indicated
-        date0 = new DatePlayed("02/10/21 12:34");
-        date1 = new DatePlayed("02/10/21");
+        date0 = new DatePlayed("2021-10-02 12:34");
+        date1 = new DatePlayed("2021-10-02");
         assertNotEquals(date0, date1);
     }
 
@@ -120,11 +115,11 @@ public class DatePlayedTest {
         DatePlayed date0;
 
         // toString does not print time if time is not specified
-        date0 = new DatePlayed("05/10/21");
+        date0 = new DatePlayed("2021-10-05");
         assertEquals(date0.toString(), "2021-10-05");
 
         // toString prints time if time is specified
-        date0 = new DatePlayed("05/10/21 11:09");
+        date0 = new DatePlayed("2021-10-05 11:09");
         assertEquals(date0.toString(), "2021-10-05 11:09");
     }
 
