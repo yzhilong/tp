6cb9f0d8a582d01e02427ba19d36341a765dd4fb
BenjaminLHR <benjamin.lee.hr@gmail.com>
2021-11-06 21:48:26 +0800

    Fix typo when specifying link to gamebook icon

--- src/test/java/seedu/gamebook/commons/util/AppUtilTest.java
+++ src/test/java/seedu/gamebook/commons/util/AppUtilTest.java
@@ -9,7 +9,7 @@ public class AppUtilTest {
 
     @Test
     public void getImage_exitingImage() {
-        assertNotNull(AppUtil.getImage("/images/gamebook_icon.png.png"));
+        assertNotNull(AppUtil.getImage("/images/gamebook_icon.png"));
     }
 
     @Test
